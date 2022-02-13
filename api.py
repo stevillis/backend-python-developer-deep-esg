dcf58073df9c2c988104daeb409cb5a259af3acb
Stévillis Sousa <stevillis@hotmail.com>
2022-02-13 17:49:27 -0400

    Create get all articles endpoint

--- api.py
+++ api.py
@@ -4,3 +4,12 @@ from data import ArticleData
 
 article_data = ArticleData()
 article_router = APIRouter()
+
+
+@article_router.get('/')
+async def get_all_articles():
+    """
+    Get all Articles\n
+    :return: A list of Articles
+    """
+    return article_data.get_all_articles()
